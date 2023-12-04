1f27015cfefc9a1be9023ac0252e9718e3a8933b
Kenneth <40445044+kennetek@users.noreply.github.com>
2023-12-03 21:37:34 -0700

    massive refactoring

--- TensorflowModelV1.py
+++ TensorflowModelV1.py
@@ -8,132 +8,176 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 from sklearn.metrics import confusion_matrix
-from tensorflow.keras import layers, models
-from tensorflow.keras.utils import to_categorical
+from keras import layers, models
+from keras.utils import to_categorical
+import tensorflow as tf
 
-# Set the path to your dataset
-data_path = "C:/Users/recla/Documents/EmbeddedCamera/train_images"
-
-# Define the width for cropping
-crop_width = 70  # Adjust this value as needed
 
 # Function to load and preprocess images
-def load_and_preprocess_data(data_path, crop_width):
-    images = []
-    labels = []
-
-    for filename in os.listdir(data_path):
-        if filename.endswith(".jpg"):
-            img = cv2.imread(os.path.join(data_path, filename), cv2.IMREAD_GRAYSCALE)
-            img = img[:, -crop_width:]  # Crop to the rightmost crop_width pixels
-            label = filename.split("_")[-1].split(".")[0]  # Extract label from filename
-
-            images.append(img)
-            labels.append(label)
-
-    return np.array(images), np.array(labels)
+def load_and_preprocess_data(path, crop):
+    img = []
+    lab = []
+    for file in os.listdir(path):
+        if not file.endswith(".jpg"):
+            continue
+        img.append(cv2.imread(os.path.join(path, file), cv2.IMREAD_GRAYSCALE)[:, -crop:])
+        lab.append(file.split("_")[-1].split(".")[0])
+    return np.array(img), np.array(lab)
 
 
-def display_predictions(model, images, true_labels, label_encoder):
-    # Randomly select a few images for display
-    num_display_images = 3
-    selected_indices = random.sample(range(len(images)), num_display_images)
+# function to
+def display_predictions(m, img, truth, enc):
+    pred_classes = enc.inverse_transform(np.argmax(m.predict(img), axis=1))
+    idx = random.sample(range(len(img)), 3)
+    display_images(img[idx], truth[idx], pred_classes[idx])
 
-    # Predict labels for the selected images
-    predicted_probs = model.predict(images.reshape(-1, 144, crop_width, 1))
-    predicted_labels = np.argmax(predicted_probs, axis=1)
-    predicted_class_names = label_encoder.inverse_transform(predicted_labels)
 
-    # Display the images and predictions
-    fig, axes = plt.subplots(1, num_display_images, figsize=(15, 5))
+def display_random(num):
+    idx = random.sample(range(len(images)), num)
+    display_images(images[idx], labels[idx])
 
-    for i, idx in enumerate(selected_indices):
-        axes[i].imshow(images[idx], cmap="gray")
-        axes[i].set_title(
-            f"True: {true_labels[idx]}\nPredicted: {predicted_class_names[idx]}"
-        )
+
+def display_images(img, true, pred=None):
+    fig, axes = plt.subplots(1, len(img), figsize=(15, 5))
+
+    for i in range(len(img)):
+        axes[i].imshow(img[i], cmap="gray")
+        if pred is None:
+            axes[i].set_title(f"Class: {true[i]}")
+        else:
+            axes[i].set_title(f"True: {true[i]}\nPred:{pred[i]}")
         axes[i].axis("off")
 
     plt.show()
 
 
+def display_confusion(pred, test, enc):
+    y_pred = np.argmax(pred, axis=1)
+    y_true = np.argmax(test, axis=1)
+    names = enc.classes_
+    cm = pd.DataFrame(confusion_matrix(y_true, y_pred), index=names, columns=names)
+
+    plt.figure(figsize=(8, 6))
+    sns.heatmap(cm, annot=True, fmt="d", cmap="Blues", cbar=False)
+    plt.title("Confusion Matrix")
+    plt.xlabel("Predicted")
+    plt.ylabel("True")
+    plt.show()
+
+
+def display_history(fit):
+    # used plot method from class slides
+    loss = fit.history['loss']
+    val_loss = fit.history['val_loss']
+    epochs = range(1, len(loss) + 1)
+    plt.plot(epochs, loss, 'g.', label='Training Loss')
+    plt.plot(epochs, val_loss, 'b', label='Validation Loss')
+    plt.title(f'Training and Validation Loss')
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.legend()
+    plt.show()
+
+
+# Function: Convert some hex value into an array for C programming
+def hex_to_c_array(hex_data, var_name):
+    c_str = ''
+
+    # Create header guard
+    c_str += '#ifndef ' + var_name.upper() + '_H\n'
+    c_str += '#define ' + var_name.upper() + '_H\n\n'
+
+    # Add array length at top of file
+    c_str += '\nunsigned int ' + var_name + '_len = ' + str(len(hex_data)) + ';\n'
+
+    # Declare C variable
+    c_str += 'unsigned char ' + var_name + '[] = {'
+    hex_array = []
+    for i, val in enumerate(hex_data):
+
+        # Construct string from hex
+        hex_str = format(val, '#04x')
+
+        # Add formatting so each line stays within 80 characters
+        if (i + 1) < len(hex_data):
+            hex_str += ','
+        if (i + 1) % 12 == 0:
+            hex_str += '\n '
+        hex_array.append(hex_str)
+
+    # Add closing brace
+    c_str += '\n ' + format(' '.join(hex_array)) + '\n};\n\n'
+
+    # Close out header guard
+    c_str += '#endif //' + var_name.upper() + '_H'
+
+    return c_str
+
+
+# Write TFLite model to a C source (or header) file
+def write_model(m):
+    converter = tf.lite.TFLiteConverter.from_keras_model(m)
+    model_tflite = converter.convert()
+    with open('model' + '.h', 'w') as file:
+        file.write(hex_to_c_array(model_tflite, 'model.h'))
+
+
+# Set the path to your dataset
+data_path = "./unique_images"
+
+# Define the width for cropping
+crop_width = 70  # Adjust this value as needed
+
 # Load and preprocess data
 images, labels = load_and_preprocess_data(data_path, crop_width)
 
-# Display a random preview of a few cropped images
-num_preview_images = 3
-fig, axes = plt.subplots(1, num_preview_images, figsize=(15, 5))
-
-for i in range(num_preview_images):
-    random_index = np.random.randint(0, len(images))
-    axes[i].imshow(images[random_index], cmap="gray")
-    axes[i].set_title(f"Class: {labels[random_index]}")
-    axes[i].axis("off")
-
-plt.show()
+# show some of the images as a test
+display_random(3)
 
 # Encode labels
-label_encoder = LabelEncoder()
-encoded_labels = label_encoder.fit_transform(labels)
+encoder = LabelEncoder()
 
 # Convert labels to one-hot encoding
-one_hot_labels = to_categorical(encoded_labels)
+one_hot_labels = to_categorical(encoder.fit_transform(labels))
+
+# reshape the images
+images = images.reshape(-1, 144, crop_width, 1)
 
 # Split the data into train, test, and validation sets
-X_train, X_temp, y_train, y_temp = train_test_split(
-    images, one_hot_labels, test_size=0.2, random_state=598
-)
-X_val, X_test, y_val, y_test = train_test_split(
-    X_temp, y_temp, test_size=0.5, random_state=598
-)
+X_train, X_temp, y_train, y_temp = train_test_split(images, one_hot_labels, test_size=0.4, random_state=598)
+X_val, X_test, y_val, y_test = train_test_split(X_temp, y_temp, test_size=0.5, random_state=598)
 
 # Build the CNN model
-model = models.Sequential()
-model.add(layers.Conv2D(32, (3, 3), activation="relu", input_shape=(144, crop_width, 1)))
-model.add(layers.MaxPooling2D((2, 2)))
-model.add(layers.Conv2D(64, (3, 3), activation="relu"))
-model.add(layers.MaxPooling2D((2, 2)))
-model.add(layers.Flatten())
-model.add(layers.Dense(64, activation="relu"))
-model.add(layers.Dense(3, activation="softmax"))
+model = models.Sequential([
+    layers.Conv2D(32, (3, 3), activation="relu", input_shape=(144, crop_width, 1)),
+    layers.MaxPooling2D((2, 2)),
+    layers.Dropout(0.25),
+    layers.Conv2D(64, (3, 3), activation="relu"),
+    layers.MaxPooling2D((2, 2)),
+    layers.Dropout(0.25),
+    layers.Flatten(),
+    layers.Dense(64, activation="relu"),
+    layers.Dense(3, activation="softmax")
+])
 
 # Compile the model
-model.compile(
-    optimizer="adam", loss="categorical_crossentropy", metrics=["accuracy"]
-)
+model.compile(optimizer="adam", loss="categorical_crossentropy", metrics=["accuracy"])
 
 # Train the model
-model.fit(
-    X_train.reshape(-1, 144, crop_width, 1),
-    y_train,
-    epochs=10,
-    validation_data=(X_val.reshape(-1, 144, crop_width, 1), y_val),
-)
+fit = model.fit(X_train, y_train, epochs=50, validation_data=(X_val, y_val))
 
 # Evaluate the model on the test set
-test_loss, test_acc = model.evaluate(X_test.reshape(-1, 144, crop_width, 1), y_test)
+test_loss, test_acc = model.evaluate(X_test, y_test)
 print(f"Test accuracy: {test_acc}")
 
+# show loss history graph
+display_history(fit)
 
-display_predictions(model, X_test, label_encoder.inverse_transform(np.argmax(y_test, axis=1)), label_encoder)
+# show 3 random example images
+display_predictions(model, X_test, encoder.inverse_transform(np.argmax(y_test, axis=1)), encoder)
 
+# show confusion matrix
+display_confusion(model.predict(X_test), y_test, encoder)
 
-#%%
-# Predict labels for the test set
-y_pred = model.predict(X_test.reshape(-1, 144, crop_width, 1))
-y_pred_classes = np.argmax(y_pred, axis=1)
-y_true_classes = np.argmax(y_test, axis=1)
-
-# Create confusion matrix
-conf_mat = confusion_matrix(y_true_classes, y_pred_classes)
-
-# Plot the confusion matrix using seaborn
-class_names = label_encoder.classes_
-df_conf_mat = pd.DataFrame(conf_mat, index=class_names, columns=class_names)
-
-plt.figure(figsize=(8, 6))
-sns.heatmap(df_conf_mat, annot=True, fmt="d", cmap="Blues", cbar=False)
-plt.title("Confusion Matrix")
-plt.xlabel("Predicted")
-plt.ylabel("True")
-plt.show()
+# save c header file with tensorflow lite weights
+write_model(model)
