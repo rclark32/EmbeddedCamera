b9e990b03c496122e57a1b0662b0165ef77cc012
Kenneth <40445044+kennetek@users.noreply.github.com>
2023-12-05 20:25:09 -0700

    smallest i can get it

--- TensorflowModelV1.py
+++ TensorflowModelV1.py
@@ -150,14 +150,14 @@ X_val, X_test, y_val, y_test = train_test_split(X_temp, y_temp, test_size=0.5, r
 
 # Build the CNN model
 model = models.Sequential([
-    layers.Conv2D(16, (3, 3), activation="relu", input_shape=(144, crop_width, 1)),
+    layers.Conv2D(32, (3, 3), activation="relu", input_shape=(144, crop_width, 1)),
     layers.MaxPooling2D((2, 2)),
     layers.Dropout(0.25),
-    layers.Conv2D(32, (3, 3), activation="relu"),
+    layers.Conv2D(8, (3, 3), activation="relu"),
     layers.MaxPooling2D((2, 2)),
     layers.Dropout(0.25),
     layers.Flatten(),
-    layers.Dense(64, activation="relu"),
+    layers.Dense(16, activation="relu"),
     layers.Dense(3, activation="softmax")
 ])
 
